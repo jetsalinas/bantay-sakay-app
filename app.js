0a4ad5dc751531aebbbf3bf08eb4d0f46f0da60f
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-11 19:55:32 +0800

    Added comments to app.js

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, draggable=true)
 })
 
+// Pages
+
 const NotFound = { template: '<p>Page not found</p>' }
 const HomeComponent = { template: `<p>Hello page</p>` }
 const TrainsComponent = { template: '<p>Trains page</p>' }
 const StationsComponent = { template: '<p>Stations page</p>' }
 const NavigatorComponent = { template: '<p>Navigator page</p>' }
+
+// Routing
 
 const routes = [
     { path: '/', component: HomeComponent },
@@ -18,6 +22,8 @@ const routes = [
 ];
 
 const router = new VueRouter({ routes });
+
+// App
 
 const app = new Vue({
     router
