8f3e6c8c462e8e0aa154d07bf3488542b48d9b24
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-11 19:10:56 +0800

    Added sidenav functionality

---
+++ app.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('henlo world')
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, draggable=true)
+})
