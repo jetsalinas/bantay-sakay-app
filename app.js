c6e9f89fe6859713a9886dee2567a232cb7b7083
jetsalinas <joseenrico.salinas@gmail.com>
2018-07-14 02:50:06 +0800

    Added updater service to api

--- app.js
+++ app.js
@@ -10,27 +10,14 @@ document.querySelector('.toggle-button').addEventListener('click', function () {
     slideout.toggle();
 });
 
-
-// Navbar Station Selection Scripts
-var trainNodes = document.querySelectorAll('.train');
-
-var hideNodes = function(e) {
-    for (var i = 0; i < trainNodes.length; i++) {
-        trainNodes[i].classList.add('train-inactive')
-    }
-    e.target.classList.remove('train-inactive');
-    console.log(e.target.dataset.index);
-}
-
-for (var i = 0; i < trainNodes.length; i++) {
-    trainNodes[i].addEventListener("click", hideNodes, false);
-  }
+//Vue definitions
 
 var stationStats = new Vue({
     el: "#app-station-statistics",
     data: {
         isNorth: true,
-        selectedStation: "Baclaran",
+        selectedStation: null,
+        selectedIndex: null,
         loadNorth: 75.1,
         loadSouth: 63.2
     }
@@ -45,3 +32,69 @@ var globalStats = new Vue({
         globalCycleTime: 1.5
     }
 })
+
+var trainsUrl = 'http://localhost:5000/api/trains';
+var stationsUrl = 'http://localhost:5000/api/stations';
+var statisticsUrl = 'http://localhost:5000/api/statistics';
+
+var trainsData = [];
+var stationsData = [];
+var statisticsData = {};
+
+var updateData = function() {
+    var request = new Request(trainsUrl , { method: 'GET'});
+    fetch(request).then(response => {
+        if(response.status === 200) {
+            return response.json();
+        } else {
+            console.log("API server could not be reached");
+        }
+    }).then(data => {
+        trainsData = data;
+    }).catch(error => {
+    });
+
+    request = new Request(stationsUrl , { method: 'GET'});
+    fetch(request).then(response => {
+        if(response.status === 200) {
+            return response.json();
+        } else {
+            console.log("API server could not be reached");
+        }
+    }).then(data => {
+        stationsData = data;
+    }).catch(error => {
+    });
+
+    request = new Request(statisticsUrl , { method: 'GET'});
+    fetch(request).then(response => {
+        if(response.status === 200) {
+            return response.json();
+        } else {
+            console.log("API server could not be reached");
+        }
+    }).then(data => {
+        statisticsData = data;
+    }).catch(error => {
+    });
+}
+
+updateData();
+
+// Navbar Station Selection Scripts
+var trainNodes = document.querySelectorAll('.train');
+
+var hideNodes = function(e) {
+    for (var i = 0; i < trainNodes.length; i++) {
+        trainNodes[i].classList.add('train-inactive')
+    }
+    e.target.classList.remove('train-inactive');
+    stationsData.selectedIndex = parseInt(e.target.dataset.index);
+    stationStats.selectedStation = stationsData[stationsData.selectedIndex].name;
+    stationStats.loadNorth = Math.round(parseFloat(stationsData[stationsData.selectedIndex].loadNorth)*100);
+    stationStats.loadSouth = Math.round(parseFloat(stationsData[stationsData.selectedIndex].loadSouth)*100);
+}
+
+for (var i = 0; i < trainNodes.length; i++) {
+    trainNodes[i].addEventListener("click", hideNodes, false);
+  }
