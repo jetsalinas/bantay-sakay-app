14c0c42d91284799dcb8393e9ec1a9b09d53f608
Maded Batara III <mnbatara3@gmail.com>
2018-07-14 23:08:14 +0800

    change navbar colors

--- app.js
+++ app.js
@@ -143,7 +143,6 @@ var selectStationView = function (e) {
     stationsData.selectedIndex = parseInt(e.target.dataset.index);
     stationStats.selectedStation = stationsData[stationsData.selectedIndex].name;
     globalStats.selectedStation = stationsData[stationsData.selectedIndex].name;
-    attractionsGeneral.selectedStation = stationsData[stationsData.selectedIndex].name;
     stationStats.loadNorth = Math.round(parseFloat(stationsData[stationsData.selectedIndex].loadNorth) * 100);
     stationStats.loadSouth = Math.round(parseFloat(stationsData[stationsData.selectedIndex].loadSouth) * 100);
     menuBar.title = stationStats.selectedStation;
